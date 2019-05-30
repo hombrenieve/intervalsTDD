b7ff76919a82e1d83e60aa46bc254eacf2eed7e7
Pablo Rebaque <pablo.rebaque.diaz@gmail.com>
2019-05-30 19:43:30 +0200

    More tests on OpenInterval

--- test/OpenIntervalTest.cc
+++ test/OpenIntervalTest.cc
@@ -16,3 +16,27 @@ TEST(OpenIntervalTest, isIntersectedOverlappingLeftEqualMin) {
     OpenInterval another = OpenIntervalBuilder().min(3).max(7).build();
     EXPECT_TRUE(one.isIntersected(another));
 }
+
+TEST(OpenIntervalTest, isIntersectedOverlappingEquals) {
+    OpenInterval one = OpenIntervalBuilder().min(3).max(14).build();
+    OpenInterval another = OpenIntervalBuilder().min(3).max(14).build();
+    EXPECT_TRUE(one.isIntersected(another));
+}
+
+TEST(OpenIntervalTest, isIntersectedOverlappingByRight) {
+    OpenInterval one = OpenIntervalBuilder().min(3).max(14).build();
+    OpenInterval another = OpenIntervalBuilder().min(7).max(17).build();
+    EXPECT_TRUE(one.isIntersected(another));
+}
+
+TEST(OpenIntervalTest, isIntersectedOverlappingByBoth) {
+    OpenInterval one = OpenIntervalBuilder().min(3).max(14).build();
+    OpenInterval another = OpenIntervalBuilder().min(0).max(17).build();
+    EXPECT_TRUE(one.isIntersected(another));
+}
+
+TEST(OpenIntervalTest, isIntersectedOverlappingByInside) {
+    OpenInterval one = OpenIntervalBuilder().min(3).max(14).build();
+    OpenInterval another = OpenIntervalBuilder().min(5).max(10).build();
+    EXPECT_TRUE(one.isIntersected(another));
+}
