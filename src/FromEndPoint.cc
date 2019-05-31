764b27fba40c5d6820e6ad691b02309947c3da2d
Pablo Rebaque <pablo.rebaque.diaz@gmail.com>
2019-05-31 18:11:25 +0200

    Refactor EndPoint to cover opened interval case

---
+++ src/FromEndPoint.cc
@@ -0,0 +1,19 @@
+#include "FromEndPoint.h"
+
+FromEndPoint::FromEndPoint(double value, bool isIncluded) :
+    EndPoint(value, isIncluded)
+{ }
+
+bool FromEndPoint::isLeftWhenEquals(const EndPoint* another) const {
+    if(this->isIncluded() && another->isIncluded()) {
+        return false;
+    }
+    if(this->isIncluded()) {
+        return another->isRightOf(this);
+    }
+    return false;
+}
+
+bool FromEndPoint::isRightWhenEquals(const EndPoint* another) const {
+    return true;
+}
